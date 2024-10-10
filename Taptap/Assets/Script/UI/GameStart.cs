627fd269f9eccd0855b76b77be88620f7324173e
Chengzi-creator <1041379828@qq.com>
2024-10-11 00:41:55 +0800

    UIGameStart.cs

--- Taptap/Assets/Script/UI/GameStart.cs
+++ Taptap/Assets/Script/UI/GameStart.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject StartMasks;
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button exitButton;
+
+    private void Awake()
     {
-        
+        Start();
+        startButton.onClick.AddListener(OnstartButtonClick);
+        exitButton.onClick.AddListener(OnexitButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartMasks.SetActive(true);
+        Time.timeScale = 0f;
     }
+
+    private void OnstartButtonClick()
+    {   
+        Time.timeScale = 1f;
+        StartMasks.SetActive(false);
+        SceneManager.LoadScene("UITest");
+        enabled = false;
+    }
+    
+    private void OnexitButtonClick()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    
 }
