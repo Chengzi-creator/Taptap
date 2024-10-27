9bad34e5e88dcf972a0b52b9b89256e5225a21fd
abirdlikefish <abirdlikefish@qq.com>
2024-10-27 23:09:17 +0800

    23:09

--- Taptap/Assets/Script/Tower/BuffTower/TowerBLazor_G.cs
+++ Taptap/Assets/Script/Tower/BuffTower/TowerBLazor_G.cs
@@ -12,6 +12,21 @@ public class TowerBLazor_G : BaseBuffTower
     public override void ReInit(ITowerManager.TowerAttribute towerAttribute, Vector2Int position, int faceDirection)
     {
         base.ReInit(towerAttribute, position, faceDirection);
+        switch(faceDirection)
+        {
+            case 0:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/gR");
+                break;
+            case 1:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/gU");
+                break;
+            case 2:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/gL");
+                break;
+            case 3:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/gD");
+                break;
+        }
         // rangeVFX = VFXManager.Instance.CreateVFX_Range_Lazor(Position, this.faceDirection , color);
     }
 }
--- Taptap/Assets/Script/Tower/BuffTower/TowerBLazor_R.cs
+++ Taptap/Assets/Script/Tower/BuffTower/TowerBLazor_R.cs
@@ -12,6 +12,21 @@ public class TowerBLazor_R : BaseBuffTower
     public override void ReInit(ITowerManager.TowerAttribute towerAttribute, Vector2Int position, int faceDirection)
     {
         base.ReInit(towerAttribute, position, faceDirection);
+        switch(faceDirection)
+        {
+            case 0:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/rR");
+                break;
+            case 1:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/rU");
+                break;
+            case 2:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/rL");
+                break;
+            case 3:
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/BuffTower/rD");
+                break;
+        }
         // rangeVFX = VFXManager.Instance.CreateVFX_Range_Lazor(Position, this.faceDirection , color);
     }
 }
