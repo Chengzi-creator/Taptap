55ee75d119eb227daa1e8e84affdfbe72f3f7af7
abirdlikefish <abirdlikefish@qq.com>
2024-10-27 18:07:58 +0800

    18:07

--- Taptap/Assets/Script/Tower/DamageTower/TowerDCatapult.cs
+++ Taptap/Assets/Script/Tower/DamageTower/TowerDCatapult.cs
@@ -49,6 +49,7 @@ public class TowerDCatapult : BaseDamageTower
             lockedEnemy.RemoveFirst();
             lockedTime.RemoveFirst();
             node = lockedTime.First;
+            VFXManager.Instance.CreateVFX_Attack_Toushiqi(position , TowerManager.Instance.GetColor(position));
         }
     }
     protected override void WaitCD(float deltaTime)
