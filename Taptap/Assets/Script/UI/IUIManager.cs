5fcf2d64f7c8aef7a43b1fd5733054f7f58901d4
forestlyn <yang2797817213@qq.com>
2024-10-29 20:31:39 +0800

    修改ui，增加总波次显示

--- Taptap/Assets/Script/UI/IUIManager.cs
+++ Taptap/Assets/Script/UI/IUIManager.cs
@@ -6,7 +6,7 @@ public interface IUIManager
 {
     public void coinChange(int coinCount);//直接传入最终的coin数
 
-    public void RoundChange(int level, int round);//传入当前关卡和当前轮数
+    public void RoundChange(int level, int round,int roundCount);//传入当前关卡和当前轮数
 
     public void overMasksOn();
     //建造栏更改
